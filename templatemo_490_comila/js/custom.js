f710b11c91da0bdc1e67d38338a5a7df218d9b38
Andi Wenck <andiwenck@gmail.com>
2020-04-16 10:22:10 -0400

    none

--- templatemo_490_comila/js/custom.js
+++ templatemo_490_comila/js/custom.js
@@ -2,7 +2,7 @@
  /* jQuery Pre loader
   -----------------------------------------------*/
 $(window).load(function(){
-    $('.preloader').fadeOut(1000); // set duration in brackets    
+    $('.preloader').fadeOut(1000); // set duration in brackets
 });
 
 
@@ -30,17 +30,16 @@ $(document).ready(function() {
         } else {
           $('.go-top').fadeOut(200);
         }
-        });   
+        });
         // Animate the scroll to top
       $('.go-top').click(function(event) {
         event.preventDefault();
       $('html, body').animate({scrollTop: 0}, 300);
       })
-      
+
 
   /* wow
   -------------------------------*/
   new WOW({ mobile: false }).init();
 
   });
-
